c3cc94996ee15f208e4b9e3c78328f154c91a666
hideyuki <hidesnow0113@gmail.com>
2025-03-09 16:47:34 +0900

    追加修正2

--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -1,35 +1,33 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
-import Stripe from "stripe"
+import Stripe from "stripe";
 
-const stripe =new Stripe.env.STRIPE_SECRET_KEY!)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 //購入履歴の保存
 export async function POST(request: Request, response: Response) {
-  
-  const {sessionId} =await request.json()
-  
-    try {
-const session =await stripe.checkout.session.retrieve(sessionId )
+  const { sessionId } = await request.json();
 
-const existingPurchase=prisma.findFirst({
-    where:{
-        userId:session.client_reference_id!,
-        bookId:session.metadata?.bookId!,
-    }
-})
-if(!existingPurchase){
-    const purchase =await prisma.purchase.create({
-        data:{
-            userId:session.client_reference_id!,
-            bookId:session.metadata?.bookId!,
+  try {
+    const session = await stripe.checkout.session.retrieve(sessionId);
+
+    const existingPurchase = prisma.findFirst({
+      where: {
+        userId: session.client_reference_id!,
+        bookId: session.metadata?.bookId!,
+      },
+    });
+    if (!existingPurchase) {
+      const purchase = await prisma.purchase.create({
+        data: {
+          userId: session.client_reference_id!,
+          bookId: session.metadata?.bookId!,
         },
-    })
-    return NextResponse.json({purchase})
-} else{
-    return NextResponse.json({message:"すでに購入済みです。"})
-}
-
+      });
+      return NextResponse.json({ purchase });
+    } else {
+      return NextResponse.json({ message: "すでに購入済みです。" });
+    }
   } catch (err) {
     return NextResponse.json(err);
   }
